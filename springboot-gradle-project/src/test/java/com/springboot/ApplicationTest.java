838b111fe41b1bd12242e592d4b0c11dd02d3841
중현 <@host.docker.internal>
2021-05-31 16:30:25 +0900

    SpringBoot Basic Configuration Setting

--- springboot-gradle-project/src/test/java/com/springboot/ApplicationTest.java
+++ springboot-gradle-project/src/test/java/com/springboot/ApplicationTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootGradleProjectApplicationTests {
+class ApplicationTest {
 
 	@Test
 	void contextLoads() {
+		
+		System.out.println("test");
 	}
 
 }
