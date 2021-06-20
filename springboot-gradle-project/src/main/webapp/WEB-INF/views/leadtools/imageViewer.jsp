d957e2735ae623c094709e6c9aa1749b7d751a6f
중현 <@host.docker.internal>
2021-06-20 16:23:02 +0900

    ignore

--- springboot-gradle-project/src/main/webapp/WEB-INF/views/leadtools/imageViewer.jsp
+++ springboot-gradle-project/src/main/webapp/WEB-INF/views/leadtools/imageViewer.jsp
@@ -7,13 +7,22 @@
 	
 	<!--Import LEADTOOLS Dependencies-->
     <script type="text/javascript" src="/resources/js/LEADTOOS/lib/Leadtools.js"></script>
-    <!-- <script type="text/javascript" src="/resources/js/LEADTOOS/Leadtools.Controls.js"></script> -->
+    <script type="text/javascript" src="/resources/js/LEADTOOS/lib/Leadtools.Controls.js"></script>
 	
 	<!--Import Project Dependencies-->
     <script type="text/javascript" src="/resources/js/LEADTOOS/app.js"></script>
     
 </head>
 <body>
+    <div 
+      id="imageViewerDiv" 
+      style="width: 600px; height: 600px; background-color: darkgray"> 
+    </div>
 
+    <h3>Click and drag to pan, CTRL-Click and drag to zoom in and out</h3> 
+ 
+    <input id="fileInput" type="file" accept="image/png, image/jpeg" /> 
+ 
+    <button id="downloadButton">Save Image</button> 
 </body>
 </html>
--- springboot-gradle-project/src/main/webapp/resources/js/LEADTOOS/app.js
+++ springboot-gradle-project/src/main/webapp/resources/js/LEADTOOS/app.js
@@ -1,19 +1,136 @@
 window.onload = function () {
+	// License Key
+	var licenseUrl    = "./resources/plugins/LEADTOOLS/License/Leadtools.lic.txt";
+	var developerKey  = "20Com0NtLyAAvwus0Ce33w==";
 	
-   	// License Key
-   	var licenseUrl    = "/resources/plugins/LEADTOOLS/License/Leadtools.lic.txt";
-   	var developerKey  = "/resources/plugins/LEADTOOLS/License/Leadtools.lic.key.txt";
+	const LeadTools = new LEADTOOS(licenseUrl,developerKey);
 
-  	lt.RasterSupport.setLicenseUri(licenseUrl, developerKey, function ( 
-    	setLicenseResult 
-  	) { 
-    	// Check the status of the license 
-	    if (setLicenseResult.result) { 
-	      console.log("LEADTOOLS client license set successfully"); 
-	    } else { 
-	      var msg = "LEADTOOLS License is missing, invalid or expired\nError:\n"; 
-	      console.log(msg); 
-	      alert(msg); 
-	    } 
-  	}); 
+	LeadTools.initImageSingleViewer();
+	LeadTools.test();
+	
+	setLicense();
+
+
+	const imageViewerDiv = document.getElementById("imageViewerDiv");
+	const createOptions = new lt.Controls.ImageViewerCreateOptions(imageViewerDiv); 
+
+	this.imageViewer = new lt.Controls.ImageViewer(createOptions); 
+	this.imageViewer.viewVerticalAlignment = lt.Controls.ControlAlignment.center; 
+	this.imageViewer.viewHorizontalAlignment = lt.Controls.ControlAlignment.center; 
+	this.imageViewer.autoCreateCanvas = true; 
+
+	// Add Pan/Zoom interactive mode 
+	// Click and drag to pan, CTRL-Click and drag to zoom in and out 
+	this.imageViewer.interactiveModes.add(new lt.Controls.ImageViewerPanZoomInteractiveMode()); 
+
+	// Load an image 
+	this.imageViewer.imageUrl = "https://demo.leadtools.com/images/jpeg/cannon.jpg"; 
+
+	this.imageViewer.zoom( 
+		lt.Controls.ControlSizeMode.fit, 
+		0.9, 
+		this.imageViewer.defaultZoomOrigin 
+	);
+
+	//Input a file 
+	const fileInput = document.getElementById("fileInput"); 
+	fileInput.addEventListener("change", (event) => { 
+	  //Change the URL of the viewer image to the input file. 
+	  if (event.target.files[0] != null) 
+		this.imageViewer.imageUrl = URL.createObjectURL(event.target.files[0]); 
+	});
+
+	//Download functionality 
+	const downloadButton = document.getElementById("downloadButton"); 
+	downloadButton.addEventListener("click", () => { 
+	  	var image = new Image(); 
+	  	image.crossOrigin = "anonymous"; 
+	  	image.src = this.imageViewer.imageUrl; 
+   
+	  	document.body.append(image); 
+	  	image.onload = () => { 
+		var canvas = document.createElement("canvas"); 
+		canvas.width = image.width; 
+		canvas.height = image.height; 
+		canvas.getContext("2d").drawImage(image, 0, 0); 
+   
+		var blob; 
+		blob = canvas.toDataURL("image/jpeg"); 
+   
+		var a = document.createElement("a"); 
+		a.href = blob; 
+		a.download = "output.png"; 
+		document.body.appendChild(a); 
+		a.click(); 
+		document.body.removeChild(a); 
+	  }; 
+	  document.body.removeChild(image); 
+	}); 
+
 }; 
+
+function setLicense() {
+	
+}
+
+
+const LEADTOOS = class {
+
+	constructor(licenseUrl,developerKey){
+		this.licenseUrl = licenseUrl;
+		this.developerKey = developerKey;
+	}
+
+	initImageSingleViewer() {
+		// License Key
+		// var licenseUrl    = "http://127.0.0.1:5500/License/Leadtools.lic.txt";
+		// var developerKey  = "20Com0NtLyAAvwus0Ce33w==";
+
+		lt.RasterSupport.setLicenseUri(this.licenseUrl, this.developerKey, function (setLicenseResult) { 
+			// Check the status of the license 
+			if (setLicenseResult.result) { 
+				console.log("LEADTOOLS client license set successfully"); 
+			} else { 
+				var msg = "LEADTOOLS License is missing, invalid or expired\nError:\n"; 
+				console.log(msg);
+			}
+		});
+	}
+
+	test() {
+		
+		function ccc() {
+
+
+		}
+
+		// aaa = () => {
+		// 	console.log("aaa");
+		// }
+
+		var ddd = () => {
+			console.log("ddd");
+		}
+	}
+}
+
+
+
+/*const promise = new Promise((a,b) => {
+	if(a == 1){
+		a("성공");
+	} else {
+		b("실패");
+	}
+});
+
+promise
+.then((message) => {
+	console.log(message);
+})
+.then((error) => {
+	console.log(error);
+})
+.finally(() => {
+	console.log('finally');
+})*/
